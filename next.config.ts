51948b8ed7ba576a75a6e2b0b0615d918d9f0a33
Aniruddha Nag <aniruddhanag780@gmail.com>
2025-11-30 17:38:07 +0000

    Okay how can I add google rechapta service?

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   env: {
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+  }
 };
 
 export default nextConfig;
