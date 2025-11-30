5e1764adc03eaa02739d3340854f63e2c6680d3d
Aniruddha Nag <aniruddhanag780@gmail.com>
2025-11-30 15:19:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7194397958-f4047",
+  "appId": "1:267662121056:web:8542ced5b6c93b0f3dcb05",
+  "apiKey": "AIzaSyA-xdnSBkP7dRUTUUZnjUuk1BYB-PoSszg",
+  "authDomain": "studio-7194397958-f4047.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "267662121056"
+};
